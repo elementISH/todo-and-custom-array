acd7566cfb5225128aa2e33bac8102e50d30ef81
ISH <70657630+elementISH@users.noreply.github.com>
2022-02-15 21:45:51 +0200

    Update app2.js

--- scripts/app2.js
+++ scripts/app2.js
@@ -1,7 +1,7 @@
 class CArray{
     constructor(){
         this.size = 0;
-        this.items = "items:";
+        this.items = "items:\n";
         // this.arr = Array.prototype.push.call(this, "")
     }
 
@@ -12,21 +12,21 @@ class CArray{
         if(Array.isArray(elements)){
             elements.forEach(element => {
                 this.size++;
-                this.items += " " + element ;
+                this.items += "- " + element + "\n";
             });
         }else{
             this.size++;
-            this.items += " " + elements;
+            this.items += "- " + elements + "\n";
         }
     }
 
     clear(){
         this.size = 0;
-        this.items = "items:";
+        this.items = "items:\n";
     }
     
     list(){
-        console.log(this.items.replaceAll(" ", " - ").replace(" - ", " "))
+        console.log(this.items)
     }
 
     search(word){
@@ -42,11 +42,12 @@ class CArray{
     remove(word){
         if(!word) return console.log("specify a word");
         let i = this.items.search(word); 
-        this.items = this.items.replace(" " + word, "")
+        this.items = this.items.replace("- " + word + "\n", "")
         let j = this.items.search(word); 
         
         if(i >= 0){
             console.warn("word found at position: " + i + " and removed")
+            this.size--;
         }else if(j < 0){
             console.error("error: couldn't remove word (probably doesn't exist)")
         }
